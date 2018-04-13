a394b72eb7c263e8d621d9df6371b811a3c7347e
Grabot <SanderKools@gmail.com>
2018-04-13 13:52:18 +0200

    manually created the first 4 nodes with half-edges

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from pyglet.gl import *
 from PIL import Image
 import VoronoiImage as VoronoiImage
 import objects.Node as Node
-import objects.Edge as Edge
+import objects.HalfEdge as HalfEdge
 import objects.Face as Face
 import objects.Graph as Graph
 
@@ -20,19 +20,63 @@ class MainWindow(window.Window):
 		self.voronoiImage = VoronoiImage.VoronoiImage(imageName)
 
 		# Mannually add 4 nodes with triangulation edges far outside the sight to make it easy to make the delaunay and voronoi calculations.
+		node1 = Node.Node(-99999999, -99999999)
+		node2 = Node.Node(99999999, -99999999)
+		node3 = Node.Node(0, 99999999)
+		# We'll also initialize a point in the field (not 0, 0 since that's the bottom left corner)
+		node4 = Node.Node(600, 400)
+
+		face1 = Face.Face(node1, node2, node4)
+		face2 = Face.Face(node2, node3, node4)
+		face3 = Face.Face(node3, node1, node4)
+
+		halfEdge1 = HalfEdge.HalfEdge(node1, face1)
+		halfEdge2 = HalfEdge.HalfEdge(node2, face1)
+		halfEdge3 = HalfEdge.HalfEdge(node4, face1)
+		halfEdge4 = HalfEdge.HalfEdge(node2, face2)
+		halfEdge5 = HalfEdge.HalfEdge(node3, face2)
+		halfEdge6 = HalfEdge.HalfEdge(node4, face2)
+		halfEdge7 = HalfEdge.HalfEdge(node3, face3)
+		halfEdge8 = HalfEdge.HalfEdge(node1, face3)
+		halfEdge9 = HalfEdge.HalfEdge(node4, face3)
+
 		nodes = []
-		nodes.append(Node.Node(-99999999, -99999999))
-		nodes.append(Node.Node(99999999, -99999999))
-		nodes.append(Node.Node(0, 99999999))
-
-		edges = []
-		edges.append(Edge.Edge(nodes[0], nodes[1]))
-		edges.append(Edge.Edge(nodes[1], nodes[2]))
-		edges.append(Edge.Edge(nodes[2], nodes[0]))
-
+		nodes.append(node1)
+		nodes.append(node2)
+		nodes.append(node3)
+		nodes.append(node4)
 		faces = []
-		faces.append(Face.Face(nodes[0], nodes[1], nodes[2]))
-		self.graph = Graph.Graph(nodes, edges, faces)
+		faces.append(face1)
+		faces.append(face2)
+		faces.append(face3)
+		halfEdges = []
+		halfEdges.append(halfEdge1)
+		halfEdges.append(halfEdge2)
+		halfEdges.append(halfEdge3)
+		halfEdges.append(halfEdge4)
+		halfEdges.append(halfEdge5)
+		halfEdges.append(halfEdge6)
+		halfEdges.append(halfEdge7)
+		halfEdges.append(halfEdge8)
+		halfEdges.append(halfEdge9)
+
+		halfEdge1.setAdjacentEdge(halfEdge9)
+		halfEdge1.setNextEdge(halfEdge2)
+		# It is possible that there isn't a adjacent Edge. This is the case for the outer edges.
+		halfEdge2.setNextEdge(halfEdge3)
+		halfEdge3.setAdjacentEdge(halfEdge4)
+		halfEdge3.setNextEdge(halfEdge1)
+		halfEdge4.setAdjacentEdge(halfEdge3)
+		halfEdge4.setNextEdge(halfEdge5)
+		halfEdge5.setNextEdge(halfEdge6)
+		halfEdge6.setAdjacentEdge(halfEdge7)
+		halfEdge6.setNextEdge(halfEdge4)
+		halfEdge7.setAdjacentEdge(halfEdge8)
+		halfEdge7.setNextEdge(halfEdge6)
+		halfEdge8.setNextEdge(halfEdge9)
+		halfEdge9.setAdjacentEdge(halfEdge1)
+		halfEdge9.setNextEdge(halfEdge7)
+		self.graph = Graph.Graph(nodes, halfEdges, faces)
 		# The graph doesn't have any nodes or edges yet.
 
 
@@ -61,13 +105,16 @@ class MainWindow(window.Window):
 					nodeX + nodeSize, nodeY + nodeSize,
 					nodeX + nodeSize, nodeY - nodeSize
 				]))
-			# draw the edges
+			# draw the edges using the half edge data structure
 			glColor4f(0, 1, 0, 1.0)
 			for e in self.graph.getEdges():
-				nodeFrom = e.getNodeFrom()
-				nodeTo = e.getNodeTo()
-				pyglet.graphics.draw(4, GL_LINES, (
-					'v2f', (0, 0, 0, height, nodeFrom.getX(), nodeFrom.getY(), nodeTo.getX(), nodeTo.getY())))
+				adjacentEdge = e.getAdjacentEdge()
+				# It is possible that there is no adjacent edge, this is the case for the outer edges, we don't need to draw them.
+				if adjacentEdge != None:
+					nodeFrom = e.getAdjacentEdge().getNode()
+					nodeTo = e.getNode()
+					pyglet.graphics.draw(4, GL_LINES, (
+						'v2f', (0, 0, 0, height, nodeFrom.getX(), nodeFrom.getY(), nodeTo.getX(), nodeTo.getY())))
 
 			# Draw the voronoi polygons (numberOfPoints, GL_POLYGON, ('v2f', [all x,y coordinates]))
 			# pyglet.graphics.draw(8, GL_POLYGON, ('v2f', [300,300, 300,400, 400,500, 500,500, 600,400, 600,300, 500,200, 400,200]))
--- objects/HalfEdge.py
+++ objects/HalfEdge.py
@@ -1,7 +1,31 @@
 
 class HalfEdge():
-    def __init__(self, node, edge, nextEdge, face):
-        self.node = node 			# vertex at the end of the half-edge
-        self.edge = edge 			# oppositely oriented adjacent half-edge 
-        self.nextEdge = nextEdge 	# next half-edge around the face
-        self.face = face 			# face the half-edge borders
+	def __init__(self, node, face, adjacentEdge=None, nextEdge=None):
+		self.node = node 					# vertex at the end of the half-edge
+		self.face = face 					# face the half-edge borders
+		self.adjacentEdge = adjacentEdge 	# oppositely oriented adjacent half-edge 
+		self.nextEdge = nextEdge 			# next half-edge around the face
+
+	def getNode(self):
+		return self.node
+
+	def setNode(self, node):
+		self.node = node
+
+	def getFace(self):
+		return self.face
+
+	def setFace(self, face):
+		self.face = face
+
+	def getAdjacentEdge(self):
+		return self.adjacentEdge
+
+	def setAdjacentEdge(self, adjacentEdge):
+		self.adjacentEdge = adjacentEdge
+
+	def getNextEdge(self):
+		return self.nextEdge
+
+	def setNextEdge(self, nextEdge):
+		self.nextEdge = nextEdge
