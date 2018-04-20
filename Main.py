c3ee519712627ea76a53c298e2707ec59c4f7bc2
Grabot <SanderKools@gmail.com>
2018-04-20 11:23:56 +0200

    mannually flipping edges

--- Main.py
+++ Main.py
@@ -13,171 +13,178 @@ import objects.Graph as Graph
 
 
 class MainWindow(window.Window):
-    def __init__(self, width, height, imageName, name):
-        window.Window.__init__(self, width, height, name)
-        self.showEdge = False
-        self.width = width
-        self.height = height
-        self.voronoiImage = VoronoiImage.VoronoiImage(imageName)
+	def __init__(self, width, height, imageName, name):
+		window.Window.__init__(self, width, height, name)
+		self.showEdge = False
+		self.width = width
+		self.height = height
+		self.voronoiImage = VoronoiImage.VoronoiImage(imageName)
 
-        # Mannually add 4 nodes with triangulation edges far outside the sight to make it easy to make the delaunay and voronoi calculations.
-        node1 = Node.Node(-99999999, -99999999)
-        node2 = Node.Node(99999999, -99999999)
-        node3 = Node.Node(0, 99999999)
+		# Mannually add 4 nodes with triangulation edges far outside the sight to make it easy to make the delaunay and voronoi calculations.
+		node1 = Node.Node(-99999999, -99999999)
+		node2 = Node.Node(99999999, -99999999)
+		node3 = Node.Node(0, 99999999)
 
-        face1 = Face.Face(node1, node2, node3)
+		face1 = Face.Face(node1, node2, node3)
 
-        halfEdge1 = HalfEdge.HalfEdge(node1, face1)
-        halfEdge2 = HalfEdge.HalfEdge(node2, face1)
-        halfEdge3 = HalfEdge.HalfEdge(node3, face1)
+		halfEdge1 = HalfEdge.HalfEdge(node1, face1)
+		halfEdge2 = HalfEdge.HalfEdge(node2, face1)
+		halfEdge3 = HalfEdge.HalfEdge(node3, face1)
 
-        # It is possible that there isn't a adjacent Edge. This is the case for the outer edges.
-        halfEdge1.setNextEdge(halfEdge2)
-        halfEdge2.setNextEdge(halfEdge3)
-        halfEdge3.setNextEdge(halfEdge1)
+		# It is possible that there isn't a adjacent Edge. This is the case for the outer edges.
+		halfEdge1.setNextEdge(halfEdge2)
+		halfEdge2.setNextEdge(halfEdge3)
+		halfEdge3.setNextEdge(halfEdge1)
 
-        face1.setEdge(halfEdge1)
+		face1.setEdge(halfEdge1)
 
-        nodes = []
-        nodes.append(node1)
-        nodes.append(node2)
-        nodes.append(node3)
-        faces = []
-        faces.append(face1)
-        halfEdges = []
-        halfEdges.append(halfEdge1)
-        halfEdges.append(halfEdge2)
-        halfEdges.append(halfEdge3)
+		nodes = []
+		nodes.append(node1)
+		nodes.append(node2)
+		nodes.append(node3)
+		faces = []
+		faces.append(face1)
+		halfEdges = []
+		halfEdges.append(halfEdge1)
+		halfEdges.append(halfEdge2)
+		halfEdges.append(halfEdge3)
 
-        # We will select a sorta random edge that is not on the outside.
-        self.theEdgeToShow = halfEdges[2]
-        self.showFace = False
-        self.showNextEdge = False
-        self.getAdjacentEdge = False
+		# We will select a sorta random edge that is not on the outside.
+		self.theEdgeToShow = halfEdges[2]
+		self.showFace = False
+		self.showNextEdge = False
+		self.getAdjacentEdge = False
+		self.flipEdge = False
 
-        self.graph = Graph.Graph(nodes, halfEdges, faces)
+		self.graph = Graph.Graph(nodes, halfEdges, faces)
 
-    def main_loop(self):
-        clock.set_fps_limit(30)
-        nodeSize = 5
+	def main_loop(self):
+		clock.set_fps_limit(30)
+		nodeSize = 5
 
-        while not self.has_exit:
-            self.dispatch_events()
-            self.clear()
+		while not self.has_exit:
+			self.dispatch_events()
+			self.clear()
 
-            if self.showNextEdge:
-                self.showNextEdge = False
-                self.theEdgeToShow = self.theEdgeToShow.getNextEdge()
-            if self.getAdjacentEdge:
-                self.getAdjacentEdge = False
-                if self.theEdgeToShow.getAdjacentEdge() is not None:
-                    self.theEdgeToShow = self.theEdgeToShow.getAdjacentEdge()
+			if self.showNextEdge:
+				self.showNextEdge = False
+				self.theEdgeToShow = self.theEdgeToShow.getNextEdge()
+			if self.getAdjacentEdge:
+				self.getAdjacentEdge = False
+				if self.theEdgeToShow.getAdjacentEdge() is not None:
+					self.theEdgeToShow = self.theEdgeToShow.getAdjacentEdge()
 
-            # White, so reset the colour
-            glColor4f(1, 1, 1, 1)
-            gl.glLineWidth(1)
-            self.draw()
+			if self.flipEdge:
+				self.graph.manuallyFlipEdge(self.theEdgeToShow)
+				self.flipEdge = False
+				print("flipping edge :)")
 
-            gl.glEnable(gl.GL_BLEND)
-            gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-            # Draw the nodes with how you can give it a colour
-            glColor4f(1, 0, 0, 1.0)
-            for n in self.graph.getNodes():
-                nodeX = n.getX()
-                nodeY = n.getY()
-                pyglet.graphics.draw(4, GL_QUADS, ('v2f', [
-                    nodeX - nodeSize, nodeY - nodeSize,
-                    nodeX - nodeSize, nodeY + nodeSize,
-                    nodeX + nodeSize, nodeY + nodeSize,
-                    nodeX + nodeSize, nodeY - nodeSize
-                ]))
-            # draw the edges using the half edge data structure
-            glColor4f(0, 1, 0, 1.0)
-            for e in self.graph.getEdges():
-                adjacentEdge = e.getAdjacentEdge()
-                # It is possible that there is no adjacent edge, this is the case for the outer edges, we don't need to draw them.
-                if adjacentEdge != None:
-                    nodeFrom = e.getAdjacentEdge().getNode()
-                    nodeTo = e.getNode()
-                    pyglet.graphics.draw(4, GL_LINES, (
-                        'v2f', (0, 0, 0, height, nodeFrom.getX(), nodeFrom.getY(), nodeTo.getX(), nodeTo.getY())))
+			# White, so reset the colour
+			glColor4f(1, 1, 1, 1)
+			gl.glLineWidth(1)
+			self.draw()
 
-            if self.showEdge:
-                # Some visual debugging, show the edge as thicker and blue and draw the face.
-                gl.glLineWidth(5)
-                glColor4f(0, 0, 1, 1.0)
-                adjacentEdge = self.theEdgeToShow.getAdjacentEdge()
-                if adjacentEdge != None:
-                    nodeFrom = self.theEdgeToShow.getAdjacentEdge().getNode()
-                    nodeTo = self.theEdgeToShow.getNode()
-                    # print("edge name is " + self.showEdge)
-                    pyglet.graphics.draw(4, GL_LINES, (
-                        'v2f', (0, 0, 0, height, nodeFrom.getX(), nodeFrom.getY(), nodeTo.getX(), nodeTo.getY())))
+			gl.glEnable(gl.GL_BLEND)
+			gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+			# Draw the nodes with how you can give it a colour
+			glColor4f(1, 0, 0, 1.0)
+			for n in self.graph.getNodes():
+				nodeX = n.getX()
+				nodeY = n.getY()
+				pyglet.graphics.draw(4, GL_QUADS, ('v2f', [
+					nodeX - nodeSize, nodeY - nodeSize,
+					nodeX - nodeSize, nodeY + nodeSize,
+					nodeX + nodeSize, nodeY + nodeSize,
+					nodeX + nodeSize, nodeY - nodeSize
+				]))
+			# draw the edges using the half edge data structure
+			glColor4f(0, 1, 0, 1.0)
+			for e in self.graph.getEdges():
+				adjacentEdge = e.getAdjacentEdge()
+				# It is possible that there is no adjacent edge, this is the case for the outer edges, we don't need to draw them.
+				if adjacentEdge != None:
+					nodeFrom = e.getAdjacentEdge().getNode()
+					nodeTo = e.getNode()
+					pyglet.graphics.draw(4, GL_LINES, (
+						'v2f', (0, 0, 0, height, nodeFrom.getX(), nodeFrom.getY(), nodeTo.getX(), nodeTo.getY())))
 
-                if self.showFace:
-                    theFace = self.theEdgeToShow.getFace()
-                    n1 = theFace.getNode1()
-                    n2 = theFace.getNode2()
-                    n3 = theFace.getNode3()
-                    pyglet.graphics.draw(3, GL_POLYGON,
-                                         ('v2f', [n1.getX(), n1.getY(), n2.getX(), n2.getY(), n3.getX(), n3.getY()]))
+			if self.showEdge:
+				# Some visual debugging, show the edge as thicker and blue and draw the face.
+				gl.glLineWidth(5)
+				glColor4f(0, 0, 1, 1.0)
+				adjacentEdge = self.theEdgeToShow.getAdjacentEdge()
+				if adjacentEdge != None:
+					nodeFrom = self.theEdgeToShow.getAdjacentEdge().getNode()
+					nodeTo = self.theEdgeToShow.getNode()
+					# print("edge name is " + self.showEdge)
+					pyglet.graphics.draw(4, GL_LINES, (
+						'v2f', (0, 0, 0, height, nodeFrom.getX(), nodeFrom.getY(), nodeTo.getX(), nodeTo.getY())))
 
-            # Draw the voronoi polygons (numberOfPoints, GL_POLYGON, ('v2f', [all x,y coordinates]))
-            # pyglet.graphics.draw(8, GL_POLYGON, ('v2f', [300,300, 300,400, 400,500, 500,500, 600,400, 600,300, 500,200, 400,200]))
+				if self.showFace:
+					theFace = self.theEdgeToShow.getFace()
+					n1 = theFace.getNode1()
+					n2 = theFace.getNode2()
+					n3 = theFace.getNode3()
+					pyglet.graphics.draw(3, GL_POLYGON,
+										 ('v2f', [n1.getX(), n1.getY(), n2.getX(), n2.getY(), n3.getX(), n3.getY()]))
 
-            clock.tick()
-            self.flip()
+			# Draw the voronoi polygons (numberOfPoints, GL_POLYGON, ('v2f', [all x,y coordinates]))
+			# pyglet.graphics.draw(8, GL_POLYGON, ('v2f', [300,300, 300,400, 400,500, 500,500, 600,400, 600,300, 500,200, 400,200]))
 
-    def draw(self):
-        self.voronoiImage.draw()
+			clock.tick()
+			self.flip()
 
-    # Event handlers
-    def on_mouse_motion(self, x, y, dx, dy):
-        pass
+	def draw(self):
+		self.voronoiImage.draw()
 
-    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-        pass
+	# Event handlers
+	def on_mouse_motion(self, x, y, dx, dy):
+		pass
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        pass
+	def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+		pass
 
-    def on_mouse_release(self, x, y, button, modifiers):
-        nodeNew = Node.Node(x, y)
-        self.graph.addNode(nodeNew)
+	def on_mouse_press(self, x, y, button, modifiers):
+		pass
 
-    def on_key_press(self, symbol, modifiers):
-        print(symbol)
-        if symbol == 97:
-            # key press A
-            print("a pressed")
-            pressedA = True
-        elif symbol == 65307:
-            # escape key is pressed
-            exit()
-        elif symbol == 98:
-            self.showEdge = True
-        elif symbol == 118:
-            self.showEdge = False
-        elif symbol == 110:
-            self.showNextEdge = True
-        elif symbol == 102:
-            self.showFace = True
-        elif symbol == 103:
-            self.showFace = False
-        elif symbol == 116:
-            self.getAdjacentEdge = True
+	def on_mouse_release(self, x, y, button, modifiers):
+		nodeNew = Node.Node(x, y)
+		self.graph.addNode(nodeNew)
 
-    def on_key_release(self, symbol, modifiers):
-        pass
+	def on_key_press(self, symbol, modifiers):
+		if symbol == 97:
+			# key press A
+			print("a pressed")
+			pressedA = True
+		elif symbol == 65307:
+			# escape key is pressed
+			exit()
+		elif symbol == 98:
+			self.showEdge = True
+		elif symbol == 118:
+			self.showEdge = False
+		elif symbol == 110:
+			self.showNextEdge = True
+		elif symbol == 102:
+			self.showFace = True
+		elif symbol == 103:
+			self.showFace = False
+		elif symbol == 116:
+			self.getAdjacentEdge = True
+		elif symbol == 112:
+			self.flipEdge = True
+
+	def on_key_release(self, symbol, modifiers):
+		pass
 
 
 if __name__ == "__main__":
-    imageName = "Oudegracht_Utrecht_2.png"
-    imagePath = os.path.abspath(os.path.dirname(__file__))
-    imagePath = os.path.join(imagePath, 'data')
-    imagePath = os.path.join(imagePath, imageName)
-    im = Image.open(imagePath)
-    (width, height) = im.size
+	imageName = "Oudegracht_Utrecht_2.png"
+	imagePath = os.path.abspath(os.path.dirname(__file__))
+	imagePath = os.path.join(imagePath, 'data')
+	imagePath = os.path.join(imagePath, imageName)
+	im = Image.open(imagePath)
+	(width, height) = im.size
 
-    window = MainWindow(width, height, imageName, "Voronoi art project")
-    window.main_loop()
+	window = MainWindow(width, height, imageName, "Voronoi art project")
+	window.main_loop()
--- objects/Graph.py
+++ objects/Graph.py
@@ -2,172 +2,246 @@ import objects.Node as Node
 import objects.Edge as Edge
 import objects.HalfEdge as HalfEdge
 import objects.Face as Face
-
+import numpy
 
 class Graph:
-    def __init__(self, nodes=[], edges=[], faces=[]):
-        self.nodes = nodes
-        self.edges = edges
-        self.faces = faces
-        # Now we won't use it anymore, but it is nice that it works and it is nice to have.
-        self.convexHullEdges = []
+	def __init__(self, nodes=[], edges=[], faces=[]):
+		self.nodes = nodes
+		self.edges = edges
+		self.faces = faces
+		# Now we won't use it anymore, but it is nice that it works and it is nice to have.
+		self.convexHullEdges = []
 
-    def setNodes(self, nodes):
-        self.nodes = nodes
+	def setNodes(self, nodes):
+		self.nodes = nodes
 
-    def getNodes(self):
-        return self.nodes
+	def getNodes(self):
+		return self.nodes
 
-    def setEdges(self, edges):
-        self.edges = edges
+	def setEdges(self, edges):
+		self.edges = edges
 
-    def getEdges(self):
-        return self.edges
+	def getEdges(self):
+		return self.edges
 
-    def setFaces(self, faces):
-        self.faces = faces
+	def setFaces(self, faces):
+		self.faces = faces
 
-    def getFaces(self):
-        return self.faces
+	def getFaces(self):
+		return self.faces
 
-    def validEdge(self):
-        print("test edge")
+	def manuallyFlipEdge(self, edgeToFlip):
+		for e in self.edges:
+			if e == edgeToFlip:
+				# First a simple check if we can flip the edge.
+				if e.getAdjacentEdge() == None:
+					return
+				# First we find the 2 faces that the edge has.
+				e1 = e
+				e2 = e.getAdjacentEdge()
+				face1 = e1.getFace()
+				face2 = e2.getFace()
+				node1 = e1.getNextEdge().getNode()
+				node2 = e2.getNextEdge().getNode()
 
-    def inFace(self, p0, p1, p2, node):
-        Area = 0.5 * (-p1.getY() * p2.getX() + p0.getY() * (-p1.getX() + p2.getX()) + p0.getX() * (
-            p1.getY() - p2.getY()) + p1.getX() * p2.getY())
+				# Create the new Half Edge.
+				e1_1 = HalfEdge.HalfEdge(node1)
+				e2_1 = HalfEdge.HalfEdge(node2)
 
-        s = 1 / (2 * Area) * (p0.getY() * p2.getX() - p0.getX() * p2.getY() + (p2.getY() - p0.getY()) * node.getX() + (
-            p0.getX() - p2.getX()) * node.getY())
-        t = 1 / (2 * Area) * (p0.getX() * p1.getY() - p0.getY() * p1.getX() + (p0.getY() - p1.getY()) * node.getX() + (
-            p1.getX() - p0.getX()) * node.getY())
+				e1_1.setAdjacentEdge(e2_1)
+				e2_1.setAdjacentEdge(e1_1)
 
-        return s > 0 and t > 0 and 1 - s - t > 0
+				# Set the correct next edges.
+				e1_1.setNextEdge(e1.getNextEdge().getNextEdge())
+				e2_1.setNextEdge(e2.getNextEdge().getNextEdge())
 
-    def addNode(self, node):
-        # We have added a node, so we want to find out which face it is in and connect it with edges
-        for f in self.faces:
-            if self.inFace(f.getNode1(), f.getNode2(), f.getNode3(), node):
-                # The 3 edges of the face that is chosen.
-                edge1 = f.getEdge()
-                edge2 = edge1.getNextEdge()
-                edge3 = edge2.getNextEdge()
+				# The new next edge here used to be on the corner, so it's easier to take it from the other side
+				e1_1.getNextEdge().setNextEdge(e2.getNextEdge())
+				e2_1.getNextEdge().setNextEdge(e1.getNextEdge())
 
-                # Create 3 new edges. and we need to fix the other half edges to get the correct "next edge"
-                edge1_1 = HalfEdge.HalfEdge(node)
-                edge1_2 = HalfEdge.HalfEdge(f.getNode1())
+				e1_1.getNextEdge().getNextEdge().setNextEdge(e1_1)
+				e2_1.getNextEdge().getNextEdge().setNextEdge(e2_1)
 
-                edge2_1 = HalfEdge.HalfEdge(node)
-                edge2_2 = HalfEdge.HalfEdge(f.getNode2())
+				# Create the new faces.
+				node1Face1 = e1_1.getNode()
+				node2Face1 = e1_1.getNextEdge().getNode()
+				node3Face1 = e1_1.getNextEdge().getNextEdge().getNode()
+				newFace1 = Face.Face(node1Face1, node2Face1, node3Face1, e1_1)
 
-                edge3_1 = HalfEdge.HalfEdge(node)
-                edge3_2 = HalfEdge.HalfEdge(f.getNode3())
+				# Create face 2
+				node1Face2 = e2_1.getNode()
+				node2Face2 = e2_1.getNextEdge().getNode()
+				node3Face2 = e2_1.getNextEdge().getNextEdge().getNode()
+				newFace2 = Face.Face(node1Face2, node2Face2, node3Face2, e2_1)
 
-                # Set the new and correct "next edge" for all new half edges and the 3 half edges of the face.
-                edge1.setNextEdge(edge1_1)
-                edge1_1.setNextEdge(edge3_2)
-                edge3_2.setNextEdge(edge1)
+				e1_1.setFace(newFace1)
+				e2_1.setFace(newFace2)
 
-                edge2.setNextEdge(edge2_1)
-                edge2_1.setNextEdge(edge1_2)
-                edge1_2.setNextEdge(edge2)
+				# Add the new faces and also remove the old ones.
+				self.faces.append(newFace1)
+				self.faces.append(newFace2)
 
-                edge3.setNextEdge(edge3_1)
-                edge3_1.setNextEdge(edge2_2)
-                edge2_2.setNextEdge(edge3)
+				self.faces.remove(face1)
+				self.faces.remove(face2)
 
-                # Set the new and correct adjacent edges. These stay the same for the original 3 face edges, but should be set for the newly created edges.
-                edge1_1.setAdjacentEdge(edge1_2)
-                edge1_2.setAdjacentEdge(edge1_1)
-                edge2_1.setAdjacentEdge(edge2_2)
-                edge2_2.setAdjacentEdge(edge2_1)
-                edge3_1.setAdjacentEdge(edge3_2)
-                edge3_2.setAdjacentEdge(edge3_1)
+				# Do the same with the new edges.
+				self.edges.append(e1_1)
+				self.edges.append(e2_1)
 
-                # We will add the 3 newly created faces. with a half edge for each face. and set the faces on the half edges
-                # Create face 1
-                node1Face1 = edge1_1.getNode()
-                node2Face1 = edge1_1.getNextEdge().getNode()
-                node3Face1 = edge1_1.getNextEdge().getNextEdge().getNode()
-                face1 = Face.Face(node1Face1, node2Face1, node3Face1, edge1_1)
+				self.edges.remove(e1)
+				self.edges.remove(e2)
+				print("flipping the flippin edge")
 
-                # Create face 2
-                node1Face2 = edge2_1.getNode()
-                node2Face2 = edge2_1.getNextEdge().getNode()
-                node3Face2 = edge2_1.getNextEdge().getNextEdge().getNode()
-                face2 = Face.Face(node1Face2, node2Face2, node3Face2, edge2_1)
 
-                node1Face3 = edge3_1.getNode()
-                node2Face3 = edge3_1.getNextEdge().getNode()
-                node3Face3 = edge3_1.getNextEdge().getNextEdge().getNode()
-                face3 = Face.Face(node1Face3, node2Face3, node3Face3, edge3_1)
+	def incircle(self, A, B, C, D):
+		# returns True is D lies in the circumcircle of ABC
+		# This is done by determining the determinant of a matrix.
+		M = [[A.getX(), A.getY(), (pow(A.getX(), 2) + pow(A.getY(), 2)), 1],
+			 [B.getX(), B.getY(), (pow(B.getX(), 2) + pow(B.getY(), 2)), 1],
+			 [C.getX(), C.getY(), (pow(C.getX(), 2) + pow(C.getY(), 2)), 1],
+			 [D.getX(), D.getY(), (pow(D.getX(), 2) + pow(D.getY(), 2)), 1]]
+		return numpy.linalg.det(M) > 0
 
-                edge1_1.setFace(face1)
-                edge1_2.setFace(face2)
-                edge2_1.setFace(face2)
-                edge2_2.setFace(face3)
-                edge3_1.setFace(face3)
-                edge3_2.setFace(face1)
-                edge1.setFace(face1)
-                edge2.setFace(face2)
-                edge3.setFace(face3)
+	def validEdge(self):
+		print("test edge")
 
-                self.faces.append(face1)
-                self.faces.append(face2)
-                self.faces.append(face3)
+	def inFace(self, p0, p1, p2, node):
+		Area = 0.5 * (-p1.getY() * p2.getX() + p0.getY() * (-p1.getX() + p2.getX()) + p0.getX() * (
+			p1.getY() - p2.getY()) + p1.getX() * p2.getY())
 
-                # We don't have to remove edges, only add the newly created ones.
-                self.edges.append(edge1_1)
-                self.edges.append(edge1_2)
-                self.edges.append(edge2_1)
-                self.edges.append(edge2_2)
-                self.edges.append(edge3_1)
-                self.edges.append(edge3_2)
+		s = 1 / (2 * Area) * (p0.getY() * p2.getX() - p0.getX() * p2.getY() + (p2.getY() - p0.getY()) * node.getX() + (
+			p0.getX() - p2.getX()) * node.getY())
+		t = 1 / (2 * Area) * (p0.getX() * p1.getY() - p0.getY() * p1.getX() + (p0.getY() - p1.getY()) * node.getX() + (
+			p1.getX() - p0.getX()) * node.getY())
 
-                # The original face is now replaced with 3 new ones, so we will remove the original
-                self.faces.remove(f)
+		return s > 0 and t > 0 and 1 - s - t > 0
 
-        self.nodes.append(node)
+	def addNode(self, node):
+		# We have added a node, so we want to find out which face it is in and connect it with edges
+		for f in self.faces:
+			if self.inFace(f.getNode1(), f.getNode2(), f.getNode3(), node):
+				# The 3 edges of the face that is chosen.
+				edge1 = f.getEdge()
+				edge2 = edge1.getNextEdge()
+				edge3 = edge2.getNextEdge()
 
-    def getConvexHullEdges(self):
-        return self.convexHullEdges
+				# Create 3 new edges. and we need to fix the other half edges to get the correct "next edge"
+				edge1_1 = HalfEdge.HalfEdge(node)
+				edge1_2 = HalfEdge.HalfEdge(f.getNode1())
 
-    def orientation(self, p1, p2, p3):
-        return (p2.getX() - p1.getX()) * (p3.getY() - p1.getY()) - (p3.getX() - p1.getX()) * (p2.getY() - p1.getY())
+				edge2_1 = HalfEdge.HalfEdge(node)
+				edge2_2 = HalfEdge.HalfEdge(f.getNode2())
 
-    def gift_wrapping(self):
-        if len(self.nodes) < 3:
-            # Not enough nodes for a convex hull calculation
-            return
+				edge3_1 = HalfEdge.HalfEdge(node)
+				edge3_2 = HalfEdge.HalfEdge(f.getNode3())
 
-        # We are going to find the left most node in the set, this node will always be in the convex hull.
-        minX = 9999999
-        hullPoint = ""
-        for n in self.nodes:
-            if n.getX() < minX:
-                hullPoint = n
-                minX = n.getX()
+				# Set the new and correct "next edge" for all new half edges and the 3 half edges of the face.
+				edge1.setNextEdge(edge1_1)
+				edge1_1.setNextEdge(edge3_2)
+				edge3_2.setNextEdge(edge1)
 
-        endPoint = ""
-        finalPoints = []
-        while finalPoints == [] or endPoint != finalPoints[0]:
-            finalPoints.append(hullPoint)
-            endPoint = self.nodes[0]
+				edge2.setNextEdge(edge2_1)
+				edge2_1.setNextEdge(edge1_2)
+				edge1_2.setNextEdge(edge2)
 
-            for index in range(1, len(self.nodes)):
-                # We want to find the angle between the last found point (finalPoints[-1]), the currently selected endPoint and the node we're looping over.
-                # If the angle is better between the last found point and the current point we're checking (n) we will put the endPoint on that node.
-                if hullPoint == endPoint or self.orientation(finalPoints[-1], endPoint, self.nodes[index]) > 0:
-                    # The orientation is on the leftside.
-                    endPoint = self.nodes[index]
+				edge3.setNextEdge(edge3_1)
+				edge3_1.setNextEdge(edge2_2)
+				edge2_2.setNextEdge(edge3)
 
-            hullPoint = endPoint
+				# Set the new and correct adjacent edges. These stay the same for the original 3 face edges, but should be set for the newly created edges.
+				edge1_1.setAdjacentEdge(edge1_2)
+				edge1_2.setAdjacentEdge(edge1_1)
+				edge2_1.setAdjacentEdge(edge2_2)
+				edge2_2.setAdjacentEdge(edge2_1)
+				edge3_1.setAdjacentEdge(edge3_2)
+				edge3_2.setAdjacentEdge(edge3_1)
 
-        # Set the edges for the convex hull
-        self.convexHullEdges = []
-        for x in range(0, len(finalPoints) - 1):
-            edge = Edge.Edge(finalPoints[x], finalPoints[x + 1])
-            self.convexHullEdges.append(edge)
-        # The final edge
-        edge = Edge.Edge(finalPoints[-1], finalPoints[0])
-        self.convexHullEdges.append(edge)
+				# We will add the 3 newly created faces. with a half edge for each face. and set the faces on the half edges
+				# Create face 1
+				node1Face1 = edge1_1.getNode()
+				node2Face1 = edge1_1.getNextEdge().getNode()
+				node3Face1 = edge1_1.getNextEdge().getNextEdge().getNode()
+				face1 = Face.Face(node1Face1, node2Face1, node3Face1, edge1_1)
+
+				# Create face 2
+				node1Face2 = edge2_1.getNode()
+				node2Face2 = edge2_1.getNextEdge().getNode()
+				node3Face2 = edge2_1.getNextEdge().getNextEdge().getNode()
+				face2 = Face.Face(node1Face2, node2Face2, node3Face2, edge2_1)
+
+				node1Face3 = edge3_1.getNode()
+				node2Face3 = edge3_1.getNextEdge().getNode()
+				node3Face3 = edge3_1.getNextEdge().getNextEdge().getNode()
+				face3 = Face.Face(node1Face3, node2Face3, node3Face3, edge3_1)
+
+				edge1_1.setFace(face1)
+				edge1_2.setFace(face2)
+				edge2_1.setFace(face2)
+				edge2_2.setFace(face3)
+				edge3_1.setFace(face3)
+				edge3_2.setFace(face1)
+				edge1.setFace(face1)
+				edge2.setFace(face2)
+				edge3.setFace(face3)
+
+				self.faces.append(face1)
+				self.faces.append(face2)
+				self.faces.append(face3)
+
+				# We don't have to remove edges, only add the newly created ones.
+				self.edges.append(edge1_1)
+				self.edges.append(edge1_2)
+				self.edges.append(edge2_1)
+				self.edges.append(edge2_2)
+				self.edges.append(edge3_1)
+				self.edges.append(edge3_2)
+
+				# The original face is now replaced with 3 new ones, so we will remove the original
+				self.faces.remove(f)
+
+		self.nodes.append(node)
+
+		
+
+	def getConvexHullEdges(self):
+		return self.convexHullEdges
+
+	def orientation(self, p1, p2, p3):
+		return (p2.getX() - p1.getX()) * (p3.getY() - p1.getY()) - (p3.getX() - p1.getX()) * (p2.getY() - p1.getY())
+
+	def gift_wrapping(self):
+		if len(self.nodes) < 3:
+			# Not enough nodes for a convex hull calculation
+			return
+
+		# We are going to find the left most node in the set, this node will always be in the convex hull.
+		minX = 9999999
+		hullPoint = ""
+		for n in self.nodes:
+			if n.getX() < minX:
+				hullPoint = n
+				minX = n.getX()
+
+		endPoint = ""
+		finalPoints = []
+		while finalPoints == [] or endPoint != finalPoints[0]:
+			finalPoints.append(hullPoint)
+			endPoint = self.nodes[0]
+
+			for index in range(1, len(self.nodes)):
+				# We want to find the angle between the last found point (finalPoints[-1]), the currently selected endPoint and the node we're looping over.
+				# If the angle is better between the last found point and the current point we're checking (n) we will put the endPoint on that node.
+				if hullPoint == endPoint or self.orientation(finalPoints[-1], endPoint, self.nodes[index]) > 0:
+					# The orientation is on the leftside.
+					endPoint = self.nodes[index]
+
+			hullPoint = endPoint
+
+		# Set the edges for the convex hull
+		self.convexHullEdges = []
+		for x in range(0, len(finalPoints) - 1):
+			edge = Edge.Edge(finalPoints[x], finalPoints[x + 1])
+			self.convexHullEdges.append(edge)
+		# The final edge
+		edge = Edge.Edge(finalPoints[-1], finalPoints[0])
+		self.convexHullEdges.append(edge)
