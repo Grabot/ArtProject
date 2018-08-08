abb9b40e9b9aa8621b85bdfae2404ce79aa578d5
Grabot <SanderKools@gmail.com>
2018-08-08 21:00:31 +0200

    cleanup and refactoring based on PEP8 standards

--- objects/graph.py
+++ objects/graph.py
@@ -1,7 +1,18 @@
 from random import shuffle
+import objects.graph_logic as graph_logic
 
-import numpy
-import objects.graph_logic as GraphLogic
+
+def is_in_face(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, node_x, node_y):
+    Area = 0.5 * (-p1_y * p2_x + p0_y * (-p1_x + p2_x) + p0_x * (p1_y - p2_y) + p1_x * p2_y)
+
+    s = 1 / (2 * Area) * (p0_y * p2_x - p0_x * p2_y + (p2_y - p0_y) * node_x + (p0_x - p2_x) * node_y)
+    t = 1 / (2 * Area) * (p0_x * p1_y - p0_y * p1_x + (p0_y - p1_y) * node_x + (p1_x - p0_x) * node_y)
+
+    return s > 0 and t > 0 and 1 - s - t > 0
+
+
+def orientation(p1, p2, p3):
+    return (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y)
 
 
 class Graph:
@@ -27,15 +38,9 @@ class Graph:
         shuffle(self._voronoi_edges)
         return self._voronoi_edges
 
-    def check_can_flip_edge(self, e):
-        # First a simple check if we can flip the edge.
-        if e.adjacent_edge == None:
-            return False
-        return True
-
     def flip_edge(self, e):
         # First a simple check if we can flip the edge.
-        [e1, e2, new_face1, new_face2, e1_1, e2_1] = GraphLogic.flip_edge(e)
+        [e1, e2, new_face1, new_face2, e1_1, e2_1] = graph_logic.flip_edge(e)
 
         # Add the new faces and also remove the old ones.
         self.edges.remove(e1)
@@ -59,22 +64,6 @@ class Graph:
             if e == edge:
                 return self.flip_edge(e)[0]
 
-    """
-    Returns True is D lies in the circumcircle of ABC
-    This is done by determining the determinant of a matrix.
-    """
-
-    def is_in_circle(self, A, B, C, D):
-        M = [[A.x, A.y, (pow(A.x, 2) + pow(A.y, 2)), 1],
-             [B.x, B.y, (pow(B.x, 2) + pow(B.y, 2)), 1],
-             [C.x, C.y, (pow(C.x, 2) + pow(C.y, 2)), 1],
-             [D.x, D.y, (pow(D.x, 2) + pow(D.y, 2)), 1]]
-        det_result = numpy.linalg.det(M)
-        return det_result > 0
-
-    def is_valid_edge(self, triangleNode1, triangleNode2, triangleNode3, node):
-        return not self.is_in_circle(triangleNode1, triangleNode2, triangleNode3, node)
-
     def check_flip_edge(self, flip_edges):
         print("flipping edges")
         while flip_edges:
@@ -82,7 +71,7 @@ class Graph:
 
             # It is possible that the edge has since been removed.
             if edge in self.edges:
-                if edge.adjacent_edge != None:
+                if edge.adjacent_edge is not None:
                     adjacentEdge = edge.adjacent_edge
                     A = adjacentEdge.node
                     B = adjacentEdge.next_edge.node
@@ -92,38 +81,25 @@ class Graph:
                     D = edge.next_edge.node
 
                     # Check the determinant of the point compared to the triangle
-                    if not self.is_valid_edge(A, B, C, D):
-                        # Check if we can flip the edge
-                        if self.check_can_flip_edge(edge):
-                            print("edge is not valid, flip it!")
-                            [e1_1, e2_1] = self.flip_edge(edge)
-                            # check the other edges now, except if it is an outer edge
-                            flip_edges.append(e1_1.next_edge)
-                            flip_edges.append(e1_1.next_edge.next_edge)
-                            flip_edges.append(e2_1.next_edge)
-                            flip_edges.append(e2_1.next_edge.next_edge)
-
-    def is_in_face(self, p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, node_x, node_y):
-        Area = 0.5 * (-p1_y * p2_x + p0_y * (-p1_x + p2_x) + p0_x * (
-                p1_y - p2_y) + p1_x * p2_y)
-
-        s = 1 / (2 * Area) * (p0_y * p2_x - p0_x * p2_y + (p2_y - p0_y) * node_x + (
-                p0_x - p2_x) * node_y)
-        t = 1 / (2 * Area) * (p0_x * p1_y - p0_y * p1_x + (p0_y - p1_y) * node_x + (
-                p1_x - p0_x) * node_y)
-
-        return s > 0 and t > 0 and 1 - s - t > 0
+                    if not graph_logic.is_valid_edge(A, B, C, D):
+                        print("edge is not valid, flip it!")
+                        [e1_1, e2_1] = self.flip_edge(edge)
+                        # check the other edges now, except if it is an outer edge
+                        flip_edges.append(e1_1.next_edge)
+                        flip_edges.append(e1_1.next_edge.next_edge)
+                        flip_edges.append(e2_1.next_edge)
+                        flip_edges.append(e2_1.next_edge.next_edge)
 
     def add_node(self, node):
         print("add node")
         # We have added a node, so we want to find out which face it is in and connect it with edges
         for f in self._faces:
-            if self.is_in_face(f.node1.x, f.node1.y, f.node2.x, f.node2.y, f.node3.x, f.node3.y, node.x, node.y):
+            if is_in_face(f.node1.x, f.node1.y, f.node2.x, f.node2.y, f.node3.x, f.node3.y, node.x, node.y):
                 [
                     face1, face2, face3,
                     edge1_1, edge1_2, edge2_1, edge2_2, edge3_1, edge3_2,
                     edge1, edge2, edge3
-                ] = GraphLogic.add_node(f, node)
+                ] = graph_logic.add_node(f, node)
 
                 self._faces.append(face1)
                 self._faces.append(face2)
@@ -148,12 +124,9 @@ class Graph:
         self.nodes.append(node)
         self.calculate_voronoi()
 
-    def orientation(self, p1, p2, p3):
-        return (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y)
-
     def find_check_face(self, x, y):
         for f in self._faces:
-            if self.is_in_face(f.node1.x, f.node1.y, f.node2.x, f.node2.y, f.node3.x, f.node3.y, x, y):
+            if is_in_face(f.node1.x, f.node1.y, f.node2.x, f.node2.y, f.node3.x, f.node3.y, x, y):
                 return f
 
     def test_edge(self, the_edge):
@@ -177,13 +150,11 @@ class Graph:
                 print("Dx:", D.x, "Dy:", D.y)
 
                 # Check the determinant of the point compared to the triangle
-                if not self.is_valid_edge(A, B, C, D):
-                    # Check if we can flip the edge
-                    if self.check_can_flip_edge(the_edge):
-                        return True
+                if not graph_logic.is_valid_edge(A, B, C, D):
+                    return True
         return False
 
     def calculate_voronoi(self):
-        self._voronoi_nodes = GraphLogic.calculate_voronoi_nodes(self._faces)
-        self._voronoi_edges = GraphLogic.calculate_voronoi_edges(self._faces, self.nodes)
-        GraphLogic.calculate_voronoi_faces(self.nodes)
+        self._voronoi_nodes = graph_logic.calculate_voronoi_nodes(self._faces)
+        self._voronoi_edges = graph_logic.calculate_voronoi_edges(self._faces, self.nodes)
+        graph_logic.calculate_voronoi_faces(self.nodes)
--- objects/graph_logic.py
+++ objects/graph_logic.py
@@ -11,56 +11,57 @@ def add_node(face, node):
     edge1 = face.edge
     edge2 = edge1.next_edge
     edge3 = edge2.next_edge
-    
+
     # Create 3 new edges. and we need to fix the other half edges to get the correct "next edge"
     edge1_1 = HalfEdge(node)
     edge1_2 = HalfEdge(face.node1)
-    
+
     edge2_1 = HalfEdge(node)
     edge2_2 = HalfEdge(face.node2)
-    
+
     edge3_1 = HalfEdge(node)
     edge3_2 = HalfEdge(face.node3)
-    
+
     # Set the new and correct "next edge" for all new half edges and the 3 half edges of the face.
     edge1.next_edge = edge1_1
     edge1_1.next_edge = edge3_2
     edge3_2.next_edge = edge1
-    
+
     edge2.next_edge = edge2_1
     edge2_1.next_edge = edge1_2
     edge1_2.next_edge = edge2
-    
+
     edge3.next_edge = edge3_1
     edge3_1.next_edge = edge2_2
     edge2_2.next_edge = edge3
-    
-    # Set the new and correct adjacent edges. These stay the same for the original 3 face edges, but should be set for the newly created edges.
+
+    # Set the new and correct adjacent edges.
+    # These stay the same for the original 3 face edges, but should be set for the newly created edges.
     edge1_1.adjacent_edge = edge1_2
     edge1_2.adjacent_edge = edge1_1
     edge2_1.adjacent_edge = edge2_2
     edge2_2.adjacent_edge = edge2_1
     edge3_1.adjacent_edge = edge3_2
     edge3_2.adjacent_edge = edge3_1
-    
+
     # We will add the 3 newly created faces. with a half edge for each face. and set the faces on the half edges
     # Create face 1
     node1_face1 = edge1_1.node
     node2_face1 = edge1_1.next_edge.node
     node3_face1 = edge1_1.next_edge.next_edge.node
     face1 = Face(node1_face1, node2_face1, node3_face1, edge1_1)
-    
+
     # Create face 2
     node1_face2 = edge2_1.node
     node2_face2 = edge2_1.next_edge.node
     node3_face2 = edge2_1.next_edge.next_edge.node
     face2 = Face(node1_face2, node2_face2, node3_face2, edge2_1)
-    
+
     node1_face3 = edge3_1.node
     node2_face3 = edge3_1.next_edge.node
     node3_face3 = edge3_1.next_edge.next_edge.node
     face3 = Face(node1_face3, node2_face3, node3_face3, edge3_1)
-    
+
     edge1_1.face = face1
     edge1_2.face = face2
     edge2_1.face = face2
@@ -70,7 +71,7 @@ def add_node(face, node):
     edge1.face = face1
     edge2.face = face2
     edge3.face = face3
-    
+
     return [
         face1, face2, face3,
         edge1_1, edge1_2, edge2_1, edge2_2, edge3_1, edge3_2,
@@ -84,46 +85,46 @@ def flip_edge(edge):
     e2 = e1.adjacent_edge
     node1 = e1.next_edge.node
     node2 = e2.next_edge.node
-    
+
     # Create the new Half Edge.
     e1_1 = HalfEdge(node1)
     e2_1 = HalfEdge(node2)
-    
+
     e1_1.adjacent_edge = e2_1
     e2_1.adjacent_edge = e1_1
-    
+
     # Set the correct next edges.
     e1_1.next_edge = e1.next_edge.next_edge
     e2_1.next_edge = e2.next_edge.next_edge
-    
+
     # The new next edge here used to be on the corner, so it's easier to take it from the other side
     e1_1.next_edge.next_edge = e2.next_edge
     e2_1.next_edge.next_edge = e1.next_edge
-    
+
     e1_1.next_edge.next_edge.next_edge = e1_1
     e2_1.next_edge.next_edge.next_edge = e2_1
-    
+
     # Create the new faces.
     node1_face1 = e1_1.node
     node2_face1 = e1_1.next_edge.node
     node3_face1 = e1_1.next_edge.next_edge.node
     new_face1 = Face(node1_face1, node2_face1, node3_face1, e1_1)
-    
+
     # Create face 2
     node1_face2 = e2_1.node
     node2_face2 = e2_1.next_edge.node
     node3_face2 = e2_1.next_edge.next_edge.node
     new_face2 = Face(node1_face2, node2_face2, node3_face2, e2_1)
-    
+
     e1_1.face = new_face1
     e2_1.face = new_face2
-    
+
     e1_1.next_edge.face = new_face1
     e2_1.next_edge.face = new_face2
-    
+
     e1_1.next_edge.next_edge.face = new_face1
     e2_1.next_edge.next_edge.face = new_face2
-    
+
     return [e1, e2, new_face1, new_face2, e1_1, e2_1]
 
 
@@ -188,7 +189,7 @@ def calculate_voronoi_edges(_faces, nodes):
         n.clear_voronoi_edges()
 
     for f in _faces:
-        # We will connect all the voronoi nodes with the 3 adjacent voronoi nodes in the faces of it's corresponding face.
+        # We will connect all voronoi nodes with the 3 adjacent voronoi nodes in the faces of it's corresponding face.
         face_edge1 = f.edge
         face_edge2 = face_edge1.next_edge
         face_edge3 = face_edge1.next_edge.next_edge
@@ -196,19 +197,19 @@ def calculate_voronoi_edges(_faces, nodes):
         face1 = None
         face2 = None
         face3 = None
-        if face_edge1.adjacent_edge != None:
+        if face_edge1.adjacent_edge is not None:
             face1 = face_edge1.adjacent_edge.face
 
-        if face_edge2.adjacent_edge != None:
+        if face_edge2.adjacent_edge is not None:
             face2 = face_edge2.adjacent_edge.face
 
-        if face_edge3.adjacent_edge != None:
+        if face_edge3.adjacent_edge is not None:
             face3 = face_edge3.adjacent_edge.face
 
         voronoi_edges = []
         # We now have the current face and all it's adjacent faces.
         # We have already calculated the voronoi nodes for these faces, so we can connect them
-        if face1 != None:
+        if face1 is not None:
             edge = Edge(f.get_voronoi_node(), face1.get_voronoi_node())
             voronoi_edges.append(edge)
             _voronoi_edges.append(edge)
@@ -222,8 +223,7 @@ def calculate_voronoi_edges(_faces, nodes):
             if f.node3 == face1.node1 or f.node3 == face1.node2 or f.node3 == face1.node3:
                 f.node3.add_voronoi_edge(edge)
 
-
-        if face2 != None:
+        if face2 is not None:
             edge = Edge(f.get_voronoi_node(), face2.get_voronoi_node())
             voronoi_edges.append(edge)
             _voronoi_edges.append(edge)
@@ -237,7 +237,7 @@ def calculate_voronoi_edges(_faces, nodes):
             if f.node3 == face2.node1 or f.node3 == face2.node2 or f.node3 == face2.node3:
                 f.node3.add_voronoi_edge(edge)
 
-        if face3 != None:
+        if face3 is not None:
             edge = Edge(f.get_voronoi_node(), face3.get_voronoi_node())
             voronoi_edges.append(edge)
             _voronoi_edges.append(edge)
@@ -255,7 +255,7 @@ def calculate_voronoi_edges(_faces, nodes):
 
 def calculate_voronoi_faces(nodes):
     for n in nodes:
-        if n.get_voronoi_face() == None:
+        if n.get_voronoi_face() is None:
             # Create a new Voronoi face object fo the node
             n.set_voronoi_face(VoronoiFace())
 
@@ -273,3 +273,16 @@ def calculate_voronoi_faces(nodes):
                 voronoi_face.add_node(node_to)
 
         voronoi_face.gift_wrapping()
+
+
+def is_valid_edge(A, B, C, D):
+    """
+    Returns True is D lies in the circumcircle of ABC
+    This is done by determining the determinant of a matrix.
+    """
+    M = [[A.x, A.y, (pow(A.x, 2) + pow(A.y, 2)), 1],
+         [B.x, B.y, (pow(B.x, 2) + pow(B.y, 2)), 1],
+         [C.x, C.y, (pow(C.x, 2) + pow(C.y, 2)), 1],
+         [D.x, D.y, (pow(D.x, 2) + pow(D.y, 2)), 1]]
+    det_result = numpy.linalg.det(M)
+    return not det_result > 0
